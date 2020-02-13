a5f444830eef69c319c4774386c6e5f983ca9f1c
doramart <doramart@qq.com>
2020-02-13 21:48:47 +0800

    update egg-dora-mailtemplate

--- app/controller/api/mailTemplate.js
+++ app/controller/api/mailTemplate.js
@@ -117,9 +117,9 @@ let MailTemplateController = {
                 throw new Error(ctx.__('validate_error_params'));
             }
 
+            emailSubject = emailTitle = '[' + siteName + '] ' + sendEmailInfo.title;
             // -1特指邮件群发
             if (tempkey == "-1") {
-                emailSubject = emailTitle = '[' + siteName + '] ' + sendEmailInfo.title;
                 emailContent = sendEmailInfo.content;
                 toEmail = sendEmailInfo.targets;
             } else if (tempkey == "0") {
@@ -127,15 +127,8 @@ let MailTemplateController = {
                 let oldLink = sendEmailInfo.password + '$' + sendEmailInfo.email + '$' + app.config.session_secret;
                 let newLink = ctx.helper.encrypt(oldLink, app.config.encrypt_key);
                 sendEmailInfo.token = newLink;
-                emailSubject = emailTitle = '[' + siteName + '] ' + ctx.__("label_sendEmail_activePwd_title");
                 emailContent = siteFunc.renderHtmlByKey(myTemp.content, sendEmailInfo, "email,userName,token,siteName,siteDomain");
-            } else if (tempkey == "3") {
-                emailSubject = emailTitle = '[' + siteName + '] ' + ctx.__("label_sendEmail_recieveMsg_title");
-                emailContent = siteFunc.renderHtmlByKey(myTemp.content, sendEmailInfo, 'name,email,siteName,siteDomain,phoneNum,comments');
-                toEmail = sysConfigs.siteEmail;
             } else if (tempkey == "6") {
-                emailSubject = emailTitle = '[' + siteName + '] ' + ctx.__("label_sendEmail_notice_haveMsg");
-
                 var msgDate = moment(sendEmailInfo.date).format('YYYY-MM-DD HH:mm:ss');
                 sendEmailInfo.message_author_userName = sendEmailInfo.author.userName;
                 sendEmailInfo.message_sendDate = msgDate;
@@ -143,12 +136,7 @@ let MailTemplateController = {
                 sendEmailInfo.message_content_id = sendEmailInfo.content._id;
                 emailContent = siteFunc.renderHtmlByKey(myTemp.content, sendEmailInfo, 'siteName,message_author_userName,message_sendDate,siteName,siteDomain,message_content_title,message_content_id');
                 toEmail = sendEmailInfo.replyAuthor.email;
-            } else if (tempkey == "4") {
-                emailSubject = emailTitle = '[' + siteName + '] ' + ctx.__("label_sendEmail_noticeuser_askInfo_success");
-                emailContent = siteFunc.renderHtmlByKey(myTemp.content, sendEmailInfo, 'name,email,siteName,siteDomain,phoneNum,comments');
-                toEmail = sendEmailInfo.email;
             } else if (tempkey == "8") {
-                emailSubject = emailTitle = '[' + siteName + '] ' + ctx.__("label_sendEmail_sendMessageCode_success");
                 emailContent = siteFunc.renderHtmlByKey(myTemp.content, sendEmailInfo, 'email,siteName,siteDomain,msgCode');
                 toEmail = sendEmailInfo.email;
             }
